9f54c306e01c1f56a5bee9d5a874cccc1233f376
Vybhav Amps <vybhav573@gmail.com>
2023-11-22 20:41:14 -0500

    Update multicode.py

--- multicode.py
+++ multicode.py
@@ -15,6 +15,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.callbacks import get_openai_callback
+from database import initialize_database,clear_history
 
 st.markdown("""
 <style>
@@ -38,51 +39,6 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-def initialize_database():
-    conn = sqlite3.connect('temp3.db')
-    cursor = conn.cursor()
-
-    cursor.execute("PRAGMA table_info(chat_bot)")
-    columns = [info[1] for info in cursor.fetchall()]
-    if "user_input" in columns and "chatbot_id" not in columns:
-       
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS new_chat_bot (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                chatbot_id TEXT,
-                user_input TEXT,
-                chatbot_response TEXT
-            )
-        ''')
-        conn.commit()
-
-        cursor.execute('''
-            INSERT INTO new_chat_bot (user_input, chatbot_response)
-            SELECT user_input, chatbot_response FROM chat_bot
-        ''')
-        conn.commit()
-
-        cursor.execute("DROP TABLE chat_bot")
-        cursor.execute("ALTER TABLE new_chat_bot RENAME TO chat_bot")
-        conn.commit()
-
-    else:
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS chat_bot (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                chatbot_id TEXT,
-                user_input TEXT,
-                chatbot_response TEXT
-            )
-        ''')
-        conn.commit()
-
-    return conn, cursor
-
-
-def clear_history(conn, cursor, chatbot_id):
-    cursor.execute('DELETE FROM chat_bot WHERE chatbot_id = ?', (chatbot_id,))
-    conn.commit()
 
 def create_vector_store_from_pdf(pdf_file):
     loader = PyPDFLoader(file_path=pdf_file)
